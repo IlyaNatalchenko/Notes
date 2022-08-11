b6d81a32ac72695b21375cf563a5dd9f151864e8
Ilian <ilia.natalchenko@gmail.com>
2022-08-12 02:23:39 +0300

    HW7

--- app/src/main/java/com/example/mynotes/LinkFragment.java
+++ app/src/main/java/com/example/mynotes/LinkFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -19,6 +20,7 @@ public class LinkFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_link, container, false);
+
     }
 
     @Override
@@ -43,4 +45,5 @@ public class LinkFragment extends Fragment {
 
         return fragment;
     }
+
 }
--- app/src/main/java/com/example/mynotes/MainActivity.java
+++ app/src/main/java/com/example/mynotes/MainActivity.java
@@ -2,13 +2,19 @@ package com.example.mynotes;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button button1, button2, button3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
 
         NoteFragment1 noteFragment1 = new NoteFragment1();
 
@@ -16,6 +22,29 @@ public class MainActivity extends AppCompatActivity {
                 .beginTransaction()
                 .replace(R.id.fragment_container, noteFragment1)
                 .commit();
+    }
+    private void initViews() {
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        initOnClickListeners();
+    }
+    private void initOnClickListeners() {
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.button1) {
+            Toast.makeText(this, "1", Toast.LENGTH_SHORT).show();
+        }
+        if (view.getId() == R.id.button2) {
+            Toast.makeText(this, "2", Toast.LENGTH_SHORT).show();
+        }
+        if (view.getId() == R.id.button3) {
+            Toast.makeText(this, "3", Toast.LENGTH_SHORT).show();
+        }
     }
 
 }
